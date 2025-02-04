03de1605e6420a886ee61ac1330b73e3cad7fa74
MohawkRaphaelT <150467892+MohawkRaphaelT@users.noreply.github.com>
2025-02-04 12:47:38 -0500

    Add motion example using time

--- move-over-time/Game.cs
+++ move-over-time/Game.cs
@@ -11,14 +11,16 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
-
+        float positionX = 0;
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-
+            Window.SetTitle("Move Over Time");
+            Window.SetSize(400, 400);
+            Window.TargetFPS = 60;
         }
 
         /// <summary>
@@ -26,7 +28,17 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(Color.OffWhite);
 
+            // Update position
+            positionX += Time.DeltaTime * 100;
+            if (positionX > Window.Width)
+            {
+                positionX = 0;
+            }
+
+            Draw.FillColor = Color.Red;
+            Draw.Circle(positionX, 200, 50);
         }
     }
 
